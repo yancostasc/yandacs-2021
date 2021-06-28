390dde6a325b03fecbecb1618b45c2b589b0ec62
Yan da Costa <61210138+yancostasc@users.noreply.github.com>
2021-06-28 20:34:25 -0300

    DACS - Adicionando as files, classes, html, mvc etc.
    Refiz a criação do repositório, adicionei todos os files novamente;
    Trabalho seguindo a arquitetura mvc.

---
+++ src/main/java/br/univille/yandacs2021/Yandacs2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.yandacs2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yandacs2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yandacs2021Application.class, args);
+	}
+
+}
