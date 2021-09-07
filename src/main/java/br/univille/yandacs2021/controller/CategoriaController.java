ac813241b5f0e4ed10349ffee19e602093a92f74
Yan <yan.costa@projuris.com.br>
2021-09-07 20:15:43 -0300

    Métodos no controller da categoria e form

--- src/main/java/br/univille/yandacs2021/controller/CategoriaController.java
+++ src/main/java/br/univille/yandacs2021/controller/CategoriaController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.univille.yandacs2021.model.Categoria;
 import br.univille.yandacs2021.service.CategoriaService;
@@ -21,7 +24,29 @@ public class CategoriaController {
     @GetMapping
     public ModelAndView index(){
         List<Categoria> listaCategoria = service.getAllCategorias();
-        return new ModelAndView("categoria/index","listaCategorias",listaCategoria);
+        return new ModelAndView("categoria/index", "listaCategorias", listaCategoria);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo(@ModelAttribute Categoria categoria){
+        return new ModelAndView("categoria/form");
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView save(Categoria categoria){
+        service.save(categoria);
+        return new ModelAndView("redirect:/categoria");
+    }
+
+    @GetMapping(value = "/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Categoria categoria){
+        return new ModelAndView("categoria/form", "categoria", categoria);
+    }
+
+    @GetMapping(value = "/delete/{id}")
+    public ModelAndView delete(@PathVariable("id") Categoria categoria){
+        service.delete(categoria);
+        return new ModelAndView("redirect:/categoria");
     }
 
 }
